571ca3127a216bdea11a1b6d232904bc80f91107
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-06-09 15:53:49 +0100

    Update2 weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -8,5 +8,7 @@ elif weather == "rainy":
     print("Don't forget yout umbrella and a raincoat.")
 elif weather == "cold": 
     print("Make sure to wear a warm coat and a scraf.")
+elif weather == "cold":
+    print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
