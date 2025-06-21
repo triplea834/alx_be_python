9826a6f3d10b9a3702563a4e195ace3340fd761c
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-06-21 01:25:03 +0100

    Update bank_account.py1

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,6 +14,6 @@ class BankAccount:
         else:
             return False
 
-    def displaybalance(self):
+    def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
 
