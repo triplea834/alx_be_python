74f5a6d1687d7c1e9d6966e8442774866deca55f
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-06-21 02:26:12 +0100

    Update test_simple_calculator.py1

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,7 +12,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(-3, -7), -10)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-5, -5), 0)
         self.assertEqual(self.calc.subtract(0, 3), -3)
