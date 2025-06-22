2f4406111a975e616bc711f6b60f22c4b0d42abc
Adedotun Adegoke <135401420+triplea834@users.noreply.github.com>
2025-06-23 00:20:14 +0100

    Update class_static_methods_demo.py3

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -26,22 +26,5 @@ class Calculator:
     def get_calculation_type(cls):
         return cls.calculation_type
 
-    @staticmethod
-    def add(a, b):
-        """Add two numbers without needing class or instance data."""
-        return a + b
-
-    @staticmethod
-    def multiply(a, b):
-        """Multiply two numbers."""
-        return a * b
-
-    @classmethod
-    def circle_area(cls, radius):
-        """Calculate the area of a circle using class-level pi."""
-        return cls.pi * radius * radius
-
-    @classmethod
-    def circle_circumference(cls, radius):
-        """Calculate circumference using class-level pi."""
-        return 2 * cls.pi * radius
+   
+     
